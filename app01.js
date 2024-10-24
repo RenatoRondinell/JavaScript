ed5ffe951dc828dba907a18106c81551f69870c4
Renato Rondinell <69861422+RenatoRondinell@users.noreply.github.com>
2024-10-23 20:36:32 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvenido a la pagina de JavaScript</h1>");
+var nomb = prompt("Ingrese su nombre");
+document.write(
+  "Hola " + nomb + " es tu primer contacto con la programacion en JavaScript"
+);
